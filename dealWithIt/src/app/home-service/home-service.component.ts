ba928bb40a14c1bd72b94b2c5adee2c250cd8311
Ketan <kda59@sfu.ca>
2023-08-22 23:58:47 -0700

    home Service UI done

--- dealWithIt/src/app/home-service/home-service.component.ts
+++ dealWithIt/src/app/home-service/home-service.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomeServiceComponent {
 
+  title = "Last Question!!";
+  homeService!: string | null;
+
+  constructor() { }
+
+  onSubmit(formData: any): void {
+   this.homeService = formData.homeService;
+    console.log(formData);
+  }
+
+  goBack(): void {
+  }
+
 }
