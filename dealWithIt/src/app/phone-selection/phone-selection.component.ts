92e27f7fbe56d45d405f172c51964e63ec291d9f
Ketan <kda59@sfu.ca>
2023-08-23 23:30:50 -0700

    phone selection UI done

--- dealWithIt/src/app/phone-selection/phone-selection.component.ts
+++ dealWithIt/src/app/phone-selection/phone-selection.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class PhoneSelectionComponent {
 
+  title = 'Phone Selection'; // Title for the page
+  selectedPhone!: string | null; // Property to store the selected phone
+  iosDevices: string[] = 
+  ['iPhone 14', 'iPhone 14 Plus', 'iPhone 14 Pro', 'iPhone 14 Pro max', 'iPhone 11', 'iPhone 12', 'iPhone 13']; // List of Apple phones
+  androidDevices: string[] = 
+  ['Samsung Galaxy S21', 'Google Pixel 7', 'Samsung Galaxy S23', 'Samsung Galaxy S22']; // List of Android phones
+
+  // Method called when the phone selection changes
+  onSelectionChange() {
+    // You can access the selected phone as this.selectedPhone
+
+    
+    // If the user selects "Not Sure," you may want to clear the selectedPhone
+    if (this.selectedPhone === 'not-sure') {
+      this.selectedPhone = null;
+    }
+
+    console.log('Selected Phone:', this.selectedPhone);
+  }
+
+  onNext(){}
+
+  onGoBack(){}
 }
