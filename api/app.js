4eb1696196e3a4255d536e6ffe1891ce2f9f60fe
Ketan <dhingraketan474@gmail.com>
2023-08-28 15:01:33 -0700

    functionality added for generating unique id for a customer and customer routes added as well

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var mongoose  = require('mongoose');
 mongoose.connect('mongodb+srv://dhingraketan:1234@maincluster.5r9bj70.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true})
@@ -21,6 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +31,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/customers', customersRouter);
+app.use('/phones', phonesRouter);
+app.use('/reps', repsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api/routes/customers.js
+++ api/routes/customers.js
@@ -1,11 +1,69 @@
 var express = require('express');
 var router = express.Router();
+var asyncHandler = require('express-async-handler');
+const { v4: uuidv4 } = require('uuid');
+
 
 const customerModel = require('../models/customers.model');
+const { response } = require('../app');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getCustomer/:userID', asyncHandler(
+    async (req, res) => {
+        const userID = req.params.userID;
+        const user = await customerModel.findOne({ userID: userID });
+        res.status(200).send(user);
+    }
+))
+
+router.post('/addCustomer', async (req, res) => {
+    const { name, phoneNum, currentProvider, monthlyBill, currentData, numLines, transType, phoneChoice, dataChoice, homeService } = req.body;
+    const userID = generateUniqueId(10);
+
+    try {
+        const newCustomer = {
+            userID,
+            name,
+            phoneNum,
+            currentProvider,
+            monthlyBill,
+            currentData,
+            numLines,
+            transType,
+            phoneChoice,
+            dataChoice,
+            homeService
+        }
+
+        const response = await customerModel.create(newCustomer);
+
+        console.log('Customer added successfully: ', response);
+        res.status(200).send(userID);
+
+    } catch (error) {
+
+        if (error.code === 11000) {
+            console.log("Customer already exists");
+            return res.status(400).json({ msg: "Customer already exists" });
+        }
+        throw error;
+    }
+
 });
+
+
+
+function generateUniqueId(length) {
+    const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // Including digits and uppercase letters
+    const charLength = characters.length;
+    let id = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charLength);
+        id += characters.charAt(randomIndex);
+    }
+
+    return id;
+
+}
 
 module.exports = router;
