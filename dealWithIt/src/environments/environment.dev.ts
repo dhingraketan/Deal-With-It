7a476b9f4370ec5103d99c5203249a613966fe9f
Ketan <kda59@sfu.ca>
2023-11-08 03:33:18 -0800

    another build

--- dealWithIt/src/environments/environment.dev.ts
+++ dealWithIt/src/environments/environment.dev.ts
@@ -1,20 +1,20 @@
 export const environment = {
   production: false,
-  BASE_URL: 'https://3.17.117.149:80/',
-  CUSTOMER_BASE_URL: 'https://3.17.117.149:80/customers/',
+  BASE_URL: 'https://dealwithit.online/',
+  CUSTOMER_BASE_URL: 'https://dealwithit.online/customers/',
   CUSTOMER: {
     ADD_CUSTOMER: 'add',
     GET_CUSTOMER: 'getCustomer'
   },
 
-  PHONE_BASE_URL: 'https://3.17.117.149:80/phones/',
+  PHONE_BASE_URL: 'https://dealwithit.online/phones/',
   PHONE: {
     ADD_PHONE: 'add',
     GET_ANDROID_PHONE: 'getAndroidPhones',
     GET_IOS_PHONE: 'getIosPhones'
   },
 
-  REP_BASE_URL: 'https://3.17.117.149:80/reps/',
+  REP_BASE_URL: 'https://dealwithit.online/reps/',
   REP: {
     REP_LOGIN: 'login',
     REP_REGISTER: 'register'
