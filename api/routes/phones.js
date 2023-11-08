af921e8dee9a20b02c772e3ceadcc0d757495ec1
Ketan <kda59@sfu.ca>
2023-11-08 03:16:27 -0800

    new prod build with updated phone list

--- api/routes/phones.js
+++ api/routes/phones.js
@@ -5,10 +5,10 @@ var asyncHandler = require('express-async-handler');
 const phoneModel = require('../models/phones.model');
 
 iosDevices = [
-  'iPhone 14', 'iPhone 14 Plus', 'iPhone 14 Pro', 'iPhone 14 Pro max', 'iPhone 11', 'iPhone 12', 'iPhone 13'
+  'iPhone 15 Pro Max', 'iPhone 15 Pro', 'iPhone 15 Plus', 'iPhone 15', 'iPhone 14', 'iPhone 14 Plus', 'iPhone 13', 'iPhone 12', 'iPhone 11'
 ]; // List of Apple phones
 androidDevices = [
-  'Samsung Galaxy S23', 'Samsung Galaxy S23 Plus', 'Samsung Galaxy S23 Ultra' ,'Samsung Galaxy S22', 'Samsung Galaxy S21 FE', 'Google Pixel 7',
+  'Samsung Galaxy S23', 'Samsung Galaxy S23 Plus', 'Samsung Galaxy S23 Ultra' ,'Samsung Galaxy S22', 'Samsung Galaxy S23 FE', 'Google Pixel 8 Pro', 'Google Pixel 8', 'Google Pixel 7',
   'Samsung Galaxy A54', 'Samsung Galazy A14', 'Moto G 5G', 'Moto G Pure', 'Moto G Power' 
 ]; // List of Android phones
 
